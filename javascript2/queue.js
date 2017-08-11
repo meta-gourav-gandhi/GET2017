2ac1a160d8bbd39b25415f9b1a831ef78edba308
meta-gourav-gandhi <gourav.gandhi@metacube.com>
2017-08-11 14:17:59 +0530

    displaying message has been improved

--- javascript2/queue.js
+++ javascript2/queue.js
@@ -6,12 +6,12 @@ function enquedata(){
 	var data= document.getElementById("content");
 	if(data.value!=""){
 	myqueue[++right]=data.value;
-	
 	document.getElementById("result").innerHTML= document.getElementById("content").value + " has been added to the ";
 	}
 	else{
 		document.getElementById("result").innerHTML="Please enter some value.";
 	}
+	
 	document.getElementById("content").value="";
 		document.getElementById("content").focus();
 }
@@ -33,9 +33,9 @@ function printdata(){
 	if(left<=right){
 		var text="";
 		for(var i=left;i<=right;i++){
-			text=text +  myqueue[i] + " ";
+			text=text +  myqueue[i] + "<br>";
 		}
-		document.getElementById("result").innerHTML ="The elements are :- " + text;
+		document.getElementById("result").innerHTML ="The elements are :- <br>" + text;
 	}
 	else{
 		document.getElementById("result").innerHTML ="Queue is empty";
