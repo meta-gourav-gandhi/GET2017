6f6d8c282e113f2cdb26fd30c251dd448b301e35
meta-gourav-gandhi <gourav.gandhi@metacube.com>
2017-08-12 16:17:47 +0530

    added new function for local storage

--- javascript3/index.js
+++ javascript3/index.js
@@ -6,6 +6,10 @@ function callSum(){
 	
 	document.getElementById("includecontent").innerHTML='<object type="text/html" data="sumfunction.html" width="100%" height="500px" ></object>';
 }
+function callContact(){
+	
+	document.getElementById("includecontent").innerHTML='<object type="text/html" data="contact.html" width="100%" height="500px" ></object>';
+}
 
 window.onload=function(){
 	callWebWorker();
