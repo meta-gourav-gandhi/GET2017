1fc56bbcadd6b8b409b8b54ae4eedce263f7504e
meta-gourav-gandhi <gourav.gandhi@metacube.com>
2017-08-28 14:41:08 +0530

    added more modular approach

--- DS-4/src/main/java/com/metacube/assignment1/Main.java
+++ DS-4/src/main/java/com/metacube/assignment1/Main.java
@@ -13,56 +13,44 @@ public class Main {
 		for (int index = 0; index < numJobs; index++) {
 			Jobs job = new Jobs();
 			System.out.println("Enter Designation");
-		
-			int input=Integer.parseInt(scan.nextLine());
-			while(!(input>=1 && input<=4)){
+
+			int input = Integer.parseInt(scan.nextLine());
+			while (!(input >= 1 && input <= 4)) {
 				System.out.println("Invalid input . Enter Designation");
-				input=Integer.parseInt(scan.nextLine());
+				input = Integer.parseInt(scan.nextLine());
 			}
 			String designation = DesignationFactory.getDesignation(input);
-			
+
 			System.out.println("Enter message");
 			String message = scan.nextLine();
 			job.setDesignation(designation);
 			job.setMessage(message);
-			job.setPriority(getPriority(designation.charAt(0)));
+			job.setPriority(DesignationFactory.getPriority(designation.charAt(0)));
 			jobQueue[index] = job;
 		}
-		
+
 		HeapSort heap = new HeapSort();
 		heap.sort(jobQueue);
-		
+
 		for (int index = 0; index < numJobs; index++) {
-			System.out.println("\nDesignation : "+jobQueue[index].getDesignation()+"\tMessage : " + jobQueue[index].getMessage());
+			System.out.println("\nDesignation : "
+					+ jobQueue[index].getDesignation() + "\t\tMessage : "
+					+ jobQueue[index].getMessage());
 		}
 		scan.close();
 	}
-	
+
 	/**
 	 * 
 	 */
 	private static void showDesignation() {
-		
-		
+
 		System.out.println("1) Chair Person ");
 		System.out.println("2) Professors");
 		System.out.println("3) Graduate students");
 		System.out.println("4) Undergraduates students");
-		
+
 	}
 
-	public static int getPriority(char ch){
-		switch(ch){
-		case 'C':	
-			return 4;
-		case 'P':
-			return 3;
-		case 'G':
-			return 2;
-		case 'U':
-			return 1;
-		default:
-				return -1;
-		}
-	}
+
 }
