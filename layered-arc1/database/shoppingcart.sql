2c5b1f4f883358c51dc81ed834d5f894ec9eb612
gouravgandhitemp <ggandhi23195@gmail.com>
2017-09-27 20:36:52 +0530

    to create the backend for the shopping cart

---
+++ layered-arc1/database/shoppingcart.sql
@@ -0,0 +1,8 @@
+
+
+create table product (
+    id int AUTO_INCREMENT PRIMARY KEY,
+    name Varchar(20),
+    price double,
+    seller Varchar(10)
+);  
---
+++ layered-arc1/src/app/contactus.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector : 'contactus',
+    templateUrl : './contactus.component.html',
+    styleUrls : ['./contactus.component.css']
+})
+
+export class ContactUs {
+
+}
